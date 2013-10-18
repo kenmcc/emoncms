94613943cee6c0a2f43826ab4c16d9d4cb690ebd
ken <kenmc@pi>
2013-10-18 21:59:14 +0100

    adjusted pressure values

--- tempreader.py
+++ tempreader.py
@@ -9,7 +9,7 @@ remoteURI="http://emoncms.org/input/post.json?apikey=a6958b2d85dfdfab9406e1e786e
 
 run=True
 while run == True:
-  try:
+  	#try:
 	data=None
 	jsonStr= ""
 	print "Waiting for data"
@@ -17,22 +17,29 @@ while run == True:
 	node, len = struct.unpack("BB", data[:2])
 	now=datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
 	print now, node, len
-	if node >= 10 and node <= 20:
+	if node >= 10 and node < 20:
 		temp,batt = struct.unpack("hh", data[2:])
 		if temp > -2000 and temp < 4000:
 		  print "Got temp, batt = ", temp, batt
       	          jsonStr = "temp:"+str(float(temp/100.0))+",batt:"+str(float(batt/1000.0))
 	elif node == 3:
 	  rain,batt = struct.unpack("hh", data[2:])
-	  print "Got rain, batt = ", temp, batt
+	  print "Got rain, batt = ", rain, batt
 	  jsonStr = "rain:"+str(float(rain/100.0))+",batt:"+str(float(batt/1000.0))
-
-	
+	elif node == 20: # this is a pressure sensor 
+          print "pressure sensor"
+	  temp, batt, pressure = struct.unpack("hhi", data[2:])
+	  if temp >-2000 and temp < 40000 and pressure >900 and pressure < 1100:
+	 	print "Got temp, battery, pressure = ", temp, batt, pressure
+		jsonStr = "temp:"+str(float(temp/10.0))
+	  	jsonStr += ",batt:"+str(float(batt/1000.0))
+          	jsonStr += ",pressure:"+str(pressure)  
+		
 	if jsonStr != "":
 	  for uri in [localURI, remoteURI]:
 		url = uri+str(node)+"&json={"+jsonStr+"}"
 	  	u = urllib.urlopen(url)
 	        u.read()
-  except:
-    	print "Closing file"
-	os.close(fd)
+  	#except:
+    	#print "Closing file"
+os.close(fd)
