9d16b14c70f445c50f1d24e3df034b4965a3838a
ken <ken@minty>
2013-10-24 23:28:51 +0100

    writes updates now

--- tempreader.py
+++ tempreader.py
@@ -95,7 +95,7 @@ def writeDataToFile():
   path=fileBaseDir+"/"+str(Y)+"/"+YM+"/"+YMD+".txt"
 
   with open(path, "a") as f:
-    dataToWrite = ",".join([str(latestData["time"]),str(latestData["delay"]),str(latestData["hum_in"],str(latestData["temp_in"]),str(latestData["hum_out"]),str(latestData["temp_out"]),str(latestData["pressure"]),str(latestData["wind_avg"]),str(latestData["wind_gust"]),str(latestData["wind_dir"]),str(latestData["rain"])] )
+    dataToWrite = ",".join([str(latestData["time"]),str(latestData["delay"]),str(latestData["hum_in"]),str(latestData["temp_in"]),str(latestData["hum_out"]),str(latestData["temp_out"]),str(latestData["pressure"]),str(latestData["wind_avg"]),str(latestData["wind_gust"]),str(latestData["wind_dir"]),str(latestData["rain"])] )
     print "would write", dataToWrite
     f.write(dataToWrite+"\n")        
 
