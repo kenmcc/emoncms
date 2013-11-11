628abbacad209e98d33423d1294c6ecb92a168f7
ken <ken@minty>
2013-11-11 23:09:04 +0000

    not working yet - no humidity

--- bmpsensor/bmpsensor.ino
+++ bmpsensor/bmpsensor.ino
@@ -13,10 +13,12 @@
 
 #include <JeeLib.h> // https://github.com/jcw/jeelib
 #include <PortsBMP085.h> // Part of JeeLib
+#include "DHT.h"
+
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // interrupt handler for JeeLabs Sleepy power saving
 
-#define myNodeID 20        // RF12 node ID in the range 1-30
+#define myNodeID 2        // RF12 node ID in the range 1-30
 #define network 99       // RF12 Network group
 #define freq RF12_433MHZ  // Frequency of RFM12B module
 
@@ -25,10 +27,16 @@ ISR(WDT_vect) { Sleepy::watchdogEvent(); } // interrupt handler for JeeLabs Slee
 #define RETRY_LIMIT 5     // Maximum number of times to retry
 #define ACK_TIME 10       // Number of milliseconds to wait for an ack
 
-PortI2C i2c (2);         // BMP085 SDA to D8 and SCL to D7
-// PortI2C i2c (1);      // BMP085 SDA to D10 and SCL to D9
+//PortI2C i2c (2);         // BMP085 SDA to D8 and SCL to D7
+PortI2C i2c (1);      // BMP085 SDA to D10 and SCL to D9
 BMP085 psensor (i2c, 3); // ultra high resolution
-#define BMP085_POWER 9   // BMP085 Power pin is connected on D9
+#define BMP085_POWER 8   // BMP085 Power pin is connected on D9
+
+#define DHTPOWER 7
+#define DHTDATA 3
+#define DHTTYPE DHT11
+
+DHT dht(DHTDATA, DHTTYPE);
 
 //########################################################################################################################
 //Data Structure to be sent
@@ -38,41 +46,15 @@ BMP085 psensor (i2c, 3); // ultra high resolution
   	  int16_t temp;	// Temperature reading
   	  int supplyV;	// Supply voltage
     	  int32_t pres;	// Pressure reading
+          int32_t humidity;
  } Payload;
 
  Payload tinytx;
-
-// Wait a few milliseconds for proper ACK
- #ifdef USE_ACK
-  static byte waitForAck() {
-   MilliTimer ackTimer;
-   while (!ackTimer.poll(ACK_TIME)) {
-     if (rf12_recvDone() && rf12_crc == 0 &&
-        rf12_hdr == (RF12_HDR_DST | RF12_HDR_CTL | myNodeID))
-        return 1;
-     }
-   return 0;
-  }
- #endif
 
 //--------------------------------------------------------------------------------------------------
 // Send payload data via RF
 //-------------------------------------------------------------------------------------------------
  static void rfwrite(){
-  #ifdef USE_ACK
-   for (byte i = 0; i <= RETRY_LIMIT; ++i) {  // tx and wait for ack up to RETRY_LIMIT times
-     rf12_sleep(-1);              // Wake up RF module
-      while (!rf12_canSend())
-      rf12_recvDone();
-      rf12_sendStart(RF12_HDR_ACK, &tinytx, sizeof tinytx); 
-      rf12_sendWait(2);           // Wait for RF to finish sending while in standby mode
-      byte acked = waitForAck();  // Wait for ACK
-      rf12_sleep(0);              // Put RF module to sleep
-      if (acked) { return; }      // Return if ACK received
-  
-   Sleepy::loseSomeTime(RETRY_PERIOD * 1000);     // If no ack received wait and try again
-   }
-  #else
      rf12_sleep(-1);              // Wake up RF module
      while (!rf12_canSend())
      rf12_recvDone();
@@ -80,7 +62,6 @@ BMP085 psensor (i2c, 3); // ultra high resolution
      rf12_sendWait(2);           // Wait for RF to finish sending while in standby mode
      rf12_sleep(0);              // Put RF module to sleep
      return;
-  #endif
  }
 
 //--------------------------------------------------------------------------------------------------
@@ -109,9 +90,14 @@ long readVcc() {
 void setup() {
 
   pinMode(BMP085_POWER, OUTPUT); // set power pin for BMP085 to output
+  pinMode(DHTPOWER, OUTPUT); // set power pin for BMP085 to output
+  
   digitalWrite(BMP085_POWER, HIGH); // turn BMP085 sensor on
+  
   Sleepy::loseSomeTime(50);
   psensor.getCalibData();
+
+  digitalWrite(BMP085_POWER, LOW); // turn BMP085 sensor on
   
   rf12_initialize(myNodeID,freq,network); // Initialize RFM12 with settings defined above 
   rf12_sleep(0);                          // Put the RFM12 to sleep
@@ -119,12 +105,15 @@ void setup() {
   PRR = bit(PRTIM1); // only keep timer 0 going
   
   ADCSRA &= ~ bit(ADEN); bitSet(PRR, PRADC); // Disable the ADC to save power
-  Serial.begin(9600)  ;
-  Serial.write("inited");
 }
 
 void loop() {
 
+  digitalWrite(BMP085_POWER, HIGH); // turn BMP085 sensor on
+  digitalWrite(DHTPOWER, HIGH);
+  Sleepy::loseSomeTime(2000);
+
+  dht.begin();
   
   // Get raw temperature reading
   psensor.startMeas(BMP085::TEMP);
@@ -144,6 +133,9 @@ void loop() {
   tinytx.supplyV = readVcc(); // Get supply voltage
 
   rfwrite(); // Send data via RF 
+
+  digitalWrite(BMP085_POWER, LOW); // turn BMP085 sensor on
+  digitalWrite(DHTPOWER, LOW);
 
   Sleepy::loseSomeTime(60000); //JeeLabs power save function: enter low power mode for 60 seconds (valid range 16-65000 ms)
 
